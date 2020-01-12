25da885b11fabda8dc7de1d5534118dc80eebc33
Ettore Galli <ettore.galli@gmail.com>
2020-01-12 22:10:29 +0100

    Gli euri iniziano a dare segni di vita

--- src/lib/Lib.euros.test.ts
+++ src/lib/Lib.euros.test.ts
@@ -3,8 +3,68 @@ import { Euros } from './euros';
 test('Basic Usage / Happy path', () => {
 
    let eu = new Euros("3.14");
-   expect(eu.getValue()).toStrictEqual({ euros: 3, cents: 14 });
+   expect(eu.getValue()).toStrictEqual({ integer: 3, fraction: 140000000, scale: 9, sign: 1 });
 
 
 });
 
+test('Basic Usage / A lot of decimals', () => {
+
+   let eu = new Euros("3.1415927");
+   expect(eu.getValue()).toStrictEqual({ integer: 3, fraction: 141592700, scale: 9, sign: 1 });
+
+
+});
+
+test('Basic Usage / add', () => {
+   let test_cases = [
+      ["123.0", "123.0", { integer: 246, fraction: 0, scale: 9, sign: 1 }],
+      ["1.01", "2.02", { integer: 3, fraction: 30000000, scale: 9, sign: 1 }],
+      ["12345678.987654321", "87654321.012345678", { integer: 99999999, fraction: 999999999, scale: 9, sign: 1 }],
+      ["0.3", "0.6", { integer: 0, fraction: 900000000, scale: 9, sign: 1 }],
+      ["0.03", "0.05", { integer: 0, fraction: 80000000, scale: 9, sign: 1 }],
+      ["1.49", "1.52", { integer: 3, fraction: 10000000, scale: 9, sign: 1 }],
+      ["4.00", "-1.98", { integer: 2, fraction: 20000000, scale: 9, sign: 1 }]
+   ]
+   for (let tc of test_cases) {
+      let eu1 = new Euros(tc[0] as string);
+      let eu2 = new Euros(tc[1] as string);
+      eu1.add(eu2);
+      expect(eu1.getValue()).toStrictEqual(tc[2]);
+   }
+});
+
+test('Basic Usage / sub', () => {
+   let test_cases = [
+      ["4.00", "1.98", { integer: 2, fraction: 20000000, scale: 9, sign: 1 }],
+      ["3.01", "1.02", { integer: 1, fraction: 990000000, scale: 9, sign: 1 }],
+      ["-5", "1", { integer: 6, fraction: 0, scale: 9, sign: -1 }],
+      ["-.5", ".2", { integer: 0, fraction: 700000000, scale: 9, sign: -1 }]
+
+   ]
+   for (let tc of test_cases) {
+      console.log("TEST SUB:", tc);
+      let eu1 = new Euros(tc[0] as string);
+      let eu2 = new Euros(tc[1] as string);
+      eu1.sub(eu2);
+      expect(eu1.getValue()).toStrictEqual(tc[2]);
+   }
+
+});
+
+
+test('Basic Usage / mult', () => {
+   let test_cases = [
+      //["4.01", "7.00", { integer: 28, fraction: 70000000, scale: 9, sign: 1 }],
+      ["123.456", "789.012", { integer: 97408, fraction: 265472000, scale: 9, sign: 1 }]
+
+   ]
+   for (let tc of test_cases) {
+      console.log("TEST MULT:", tc);
+      let eu1 = new Euros(tc[0] as string);
+      let eu2 = new Euros(tc[1] as string);
+      eu1.mult(eu2);
+      expect(eu1.getValue()).toStrictEqual(tc[2]);
+   }
+
+});
--- src/lib/euros.ts
+++ src/lib/euros.ts
@@ -3,32 +3,173 @@
  */
 
 interface EurosValue {
-    euros: number,
-    cents: number
+    integer: number,
+    fraction: number,
+    scale: number,
+    sign: number
 }
 
+/**
+ * Decimali
+ * 
+ * valore = __sign * (__integer + __fraction / (10 ^ __scale))
+ */
 class Euros {
-    private __integer: number; // int
-    private __fraction: number; // int
-    private __scale: number = 100;
+
     private __SEPARATORS: RegExp = /[.,]/;
     private __DECIMAL_POINT: string = ".";
+    private __SCALE: number = 9;
+    private __sign: number; // +1 / -1
+    private __integer: number;
+    private __fraction: number;
+    private __scale: number;
+    private __SCALEMISMATCHERROR: number = 10001;
 
+    /**
+     * Costruttore
+     * @param value 
+     */
     constructor(value: string) {
+        this.__scale = this.__SCALE;
         let oneSeparatorValue: string = value.replace(this.__SEPARATORS, this.__DECIMAL_POINT);
         let parts = oneSeparatorValue.split(this.__DECIMAL_POINT);
-        this.__integer = parts[0] ? parseInt(parts[0]) : 0;
+
+
+        if (parts[0] === "") {
+            parts[0] = "0"
+            this.__integer = 0;
+            this.__sign = 1;
+        }
+        else if (parts[0] === "-") {
+            parts[0] = "-0"
+            this.__integer = 0;
+            this.__sign = -1;
+        }
+        else {
+            let intPart: number = parts[0] ? parseInt(parts[0]) : 0;
+            this.__sign = intPart < 0 ? -1 : 1;
+            this.__integer = Math.abs(intPart);
+        }
+
+        if (parts.length < 2) {
+            parts.push("0");
+        }
         this.__fraction = parts[1] ? parseInt(parts[1]) : 0;
 
+        let padExp = this.__scale - parts[1].length;
+        if (padExp > 0) {
+            this.__fraction = this.__fraction * Math.pow(10, padExp)
+        }
+    }
+
+    parseFromString(value: string) {
 
     }
 
     getValue(): EurosValue {
         return {
-            euros: this.__integer,
-            cents: this.__fraction
+            integer: this.__integer,
+            fraction: this.__fraction,
+            scale: this.__scale,
+            sign: this.__sign
         };
     }
+
+    isPositive() {
+        return this.__sign > 0;
+    }
+
+    getInteger(): number {
+        return this.__integer;
+    }
+
+    getFraction(): number {
+        return this.__fraction;
+    }
+
+    getScale(): number {
+        return this.__scale;
+    }
+
+    toString(): string {
+        return this.__integer + this.__DECIMAL_POINT + this.__fraction;
+    }
+
+    minus(): Euros {
+        this.__sign = - this.__sign;
+        return this;
+    }
+    add(addendum: Euros): Euros {
+        if (this.isPositive() && addendum.isPositive()) {
+            return this.__executeAdd(addendum);
+        }
+        else if (!this.isPositive() && !addendum.isPositive()) {
+            return this.minus().__executeAdd(addendum).minus();
+        }
+        else if (this.isPositive() && !addendum.isPositive()) {
+            return this.__executeSub(addendum.minus());
+        }
+        else { // (!this.isPositive() && addendum.isPositive())
+            return this.minus().__executeSub(addendum).minus();
+        }
+    }
+    __executeAdd(addendum: Euros): Euros {
+        if (this.__scale !== addendum.__scale) {
+            throw new Error(this.__SCALEMISMATCHERROR.toString());
+        }
+        let commonScale = this.__scale;
+        let f: number = this.__fraction + addendum.getFraction();
+        this.__fraction = f % Math.pow(10, commonScale);
+        this.__integer = this.__integer + addendum.getInteger() + Math.floor(f / Math.pow(10, commonScale));
+        return this;
+    }
+
+    sub(subtrahend: Euros): Euros {
+        return this.add(subtrahend.minus())
+    }
+    __executeSub(subtrahend: Euros): Euros {
+        let delta_fractions = this.__fraction - subtrahend.__fraction;
+        let borrow = 0;
+        if (delta_fractions < 0) {
+            borrow = 1;
+            delta_fractions = Math.pow(10, this.__scale) + delta_fractions;
+        }
+        this.__fraction = delta_fractions;
+        this.__integer = this.__integer - subtrahend.__integer - borrow;
+        return this;
+    }
+
+    getErrorMessage(code: number) {
+        return "Error" + code.toString();
+    }
+
+    mult(multiplier: Euros): Euros {
+        this.__sign = this.__sign * multiplier.__sign;
+
+        let k1 = this.__integer * multiplier.__integer;
+        let k2 = this.__integer * multiplier.__fraction;
+        let k3 = this.__fraction * multiplier.__integer;
+        let k4 = this.__fraction * multiplier.__fraction;
+
+        let s1 = k1 + Math.floor(k2 / Math.pow(10, this.__scale)) +
+            Math.floor(k3 / Math.pow(10, this.__scale)) +
+            Math.floor(k4 / Math.pow(10, 2 * this.__scale))
+            ;
+
+
+        let s2 = k2 % Math.pow(10, this.__scale) +
+            k3 % Math.pow(10, this.__scale) +
+            Math.floor(k4 % Math.pow(10, 2 * this.__scale) / Math.pow(10, this.__scale));
+        ;
+
+        this.__fraction = s2 % Math.pow(10, this.__scale);
+        this.__integer = s1 + Math.floor(s2 / Math.pow(10, this.__scale));
+
+        return this;
+    }
+
+
+
 }
 
 export { Euros };
